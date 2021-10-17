75f7bb27e17b70e7150e0824ff68c5d23c384aa2
DESKTOP-FOBJSU5\theob <514121@chrome.escg.ac.uk>
2021-10-17 23:37:59 +0100

    New project tezstr

---
+++ Assets/Scripts/TurnBasedFight.cs
@@ -0,0 +1,137 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+using System.Threading.Tasks;
+
+public class TurnBasedFight : MonoBehaviour
+{
+    public GameObject Heal;
+    public GameObject Attack; 
+    public int xHealth = 40;
+    public int oHealth = 50;
+    public Text playerOHealth;
+    public Text playerXHealth;
+    public Text turnString; 
+    public int turn = 1;
+
+
+    public string oHealthTextO = "";
+    // Start is called before the first frame update
+
+     void Awake()
+    {
+        turn = 1;
+        xHealth = 40;
+        oHealth = 50;
+        playerOHealth.text = "Your health is " + oHealth;
+        playerXHealth.text = "Your health is " + xHealth;
+    }
+
+    void Start()
+    {
+        //turn = 0; 
+        //xHealth = 40;
+        //oHealth = 50;
+        //playerOHealth.text = "Your health is " + oHealth;
+       // playerXHealth.text = "Your health is " + xHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [System.Serializable]
+    public class Player
+    {
+        public Image panel;
+        public Text text;
+        public Button button;
+    }
+
+    public void OnClickOFight()
+    {
+        
+
+       if (xHealth != 0 || oHealth != 0)
+        {
+            if (turn%2 == 0)
+            {
+                turnString.text = "X decided to attack"; 
+                oHealth -= Random.Range(0, 20);
+                if (oHealth < 0)
+                {
+                    oHealth = 0;
+                    playerOHealth.text = "O health is 0, you lost this fight";
+                    SceneManager.LoadScene("SampleScene");
+
+                }
+                playerOHealth.text = "Your health is " + oHealth;
+                turn++;
+                turnString.text = "What will O's do"; 
+            }
+            else
+            {
+                turnString.text = "O decided to attack"; 
+                xHealth -= Random.Range(0, 15);
+                playerXHealth.text = "Your health is " + xHealth;
+                turn++;
+                turnString.text = "What will X's do";
+            }    
+        }
+        else if (xHealth < 0)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+        else if (oHealth < 0)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+
+
+    }
+
+
+    public void OnClickHeal()
+    {
+        if (xHealth != 0 || oHealth != 0)
+        {
+            if (turn % 2 == 0)
+            {
+                Heal.SetActive(false);
+                Attack.SetActive(false);
+                turnString.text = "X decided to Heal";
+                oHealth += Random.Range(0, 10);
+                playerOHealth.text = "Your health is " + xHealth;
+                turn++;
+                Heal.SetActive(true);
+                Attack.SetActive(true);
+                turnString.text = "What will O's do";
+
+            }
+            else
+            {
+                Heal.SetActive(false);
+                Attack.SetActive(false);
+                turnString.text = "O decided to Heal";
+                xHealth += Random.Range(0, 10);
+                playerXHealth.text = "Your health is " + oHealth;
+                turn++;
+                Heal.SetActive(true);
+                Attack.SetActive(true);
+                turnString.text = "What will X's do";
+            }
+        }
+        else if (xHealth < 0)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+        else if (oHealth < 0)
+        {
+            SceneManager.LoadScene("SampleScene"); 
+        }
+    }
+}
